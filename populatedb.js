7d50ad772b8d9eca88ab001fe0d86499323685ff
ev0clu <ev0clu@gmail.com>
2023-06-27 11:06:28 +0200

    Script is updated

--- populatedb.js
+++ populatedb.js
@@ -10,12 +10,10 @@ const userArgs = process.argv.slice(2);
 const Movie = require('./models/Movie');
 const Director = require('./models/Director');
 const Genre = require('./models/Genre');
-const MovieStatus = require('./models/MovieStatus');
 
 const genres = [];
 const directors = [];
 const movies = [];
-const movieStatuses = [];
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false); // Prepare for Mongoose 7
@@ -31,7 +29,6 @@ async function main() {
     await createGenres();
     await createDirectors();
     await createMovies();
-    await createMovieStatuses();
     console.log('Debug: Closing mongoose');
     mongoose.connection.close();
 }
@@ -58,32 +55,20 @@ async function directorCreate(index, first_name, last_name, date_of_birth, date_
     console.log(`Added director: ${first_name} ${last_name}`);
 }
 
-async function movieCreate(index, title, summary, director, genre) {
+async function movieCreate(index, title, summary, director, genre, status, date) {
     const movieDetail = {
         title: title,
         summary: summary,
-        director: director,
-        genre: genre
+        director: director
     };
     if (genre != false) movieDetail.genre = genre;
+    if (status != false) movieDetail.status = status;
+    if (date != false) movieDetail.date = date;
 
     const movie = new Movie(movieDetail);
     await movie.save();
     movies[index] = movie;
     console.log(`Added movie: ${title}`);
-}
-
-async function movieStatusCreate(index, movie, due_date, status) {
-    const movieStatusDetail = {
-        movie: movie
-    };
-    if (due_date != false) movieStatusDetail.due_date = due_date;
-    if (status != false) movieStatusDetail.status = status;
-
-    const movieStatus = new MovieStatus(movieStatusDetail);
-    await movieStatus.save();
-    movieStatuses[index] = movieStatus;
-    console.log(`Added movie status: ${status}`);
 }
 
 async function createGenres() {
@@ -118,54 +103,54 @@ async function createMovies() {
             'The Wolf of Wall Street',
             'Based on the true story of Jordan Belfort, from his rise to a wealthy stock-broker living the high life to his fall involving crime, corruption and the federal government.',
             directors[0],
-            [genres[0], genres[1]]
+            [genres[0], genres[1]],
+            'Watched',
+            false
         ),
         movieCreate(
             1,
             'Dune: Part One',
             `A noble family becomes embroiled in a war for control over the galaxy's most valuable asset while its heir becomes troubled by visions of a dark future.`,
             directors[1],
-            [genres[2], genres[3], genres[4]]
+            [genres[2], genres[3], genres[4]],
+            false,
+            false
         ),
         movieCreate(
             2,
             'Once Upon a Time in Hollywood',
             `A faded television actor and his stunt double strive to achieve fame and success in the final years of Hollywood's Golden Age in 1969 Los Angeles.`,
             directors[2],
-            [genres[0], genres[4]]
+            [genres[0], genres[4]],
+            false,
+            false
         ),
         movieCreate(
             3,
             'Inception',
             'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
             directors[3],
-            [genres[2], genres[3], genres[5]]
+            [genres[2], genres[3], genres[5]],
+            'Watchlist',
+            false
         ),
         movieCreate(
             4,
             'Interstellar',
             'When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.',
             directors[3],
-            [genres[3], genres[4], genres[5]]
+            [genres[3], genres[4], genres[5]],
+            'Watched',
+            false
         ),
         movieCreate(
             5,
             'The Good, the Bad and the Ugly',
             'A bounty hunting scam joins two men in an uneasy alliance against a third in a race to find a fortune in gold buried in a remote cemetery.',
             directors[4],
-            [genres[3], genres[6]]
+            [genres[3], genres[6]],
+            'Watch again',
+            false
         )
-    ]);
-}
-
-async function createMovieStatuses() {
-    console.log('Adding movie statuses');
-    await Promise.all([
-        movieStatusCreate(0, movies[0], false, 'Watched'),
-        movieStatusCreate(1, movies[1], false, false),
-        movieStatusCreate(2, movies[2], false, false),
-        movieStatusCreate(3, movies[3], false, 'Watchlist'),
-        movieStatusCreate(4, movies[4], false, 'Watched'),
-        movieStatusCreate(5, movies[5], false, 'Watch again')
     ]);
 }
